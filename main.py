d6462848cc60b632ad72ca59dc7cfceee4f783c4
Anika <88386880+anikagupte@users.noreply.github.com>
2026-01-24 16:55:40 +0000

    updated code for new robot

--- main.py
+++ main.py
@@ -11,10 +11,8 @@ right_motor = Motor(Ports.PORT12, GearSetting.RATIO_36_1, True)
 conveyor_motor = Motor(Ports.PORT3, GearSetting.RATIO_36_1, False)
 intake_motor = Motor(Ports.PORT1, GearSetting.RATIO_36_1, True)
 
-
 # wait for rotation sensor to fully initialize
 wait(30, MSEC)
-
 
 # Make random actually random
 def initializeRandomSeed():
@@ -24,7 +22,6 @@ def initializeRandomSeed():
       
 # Set random seed 
 initializeRandomSeed()
-
 
 def play_vexcode_sound(sound_name):
     # Helper to make playing sounds from the V5 in VEXcode easier and
@@ -37,66 +34,38 @@ wait(200, MSEC)
 # clear the console to make sure we don't have the REPL in the console
 print("\033[2J")
 
-#endregion VEXcode Generated Robot Configuration
-
-# ------------------------------------------
-# 
-# 	Project:      VEXcode Project
-#	Author:       VEX
-#	Created:
-#	Description:  VEXcode V5 Python Project
-# 
-# ------------------------------------------
-
-# Library imports
-from vex import *
-
 # Begin project code
-"""
-def pre_autonomous():
-    # actions to do when the program starts
-    brain.screen.clear_screen()
-    brain.screen.print("pre auton code")
-    wait(1, SECONDS)
 
-def autonomous():
-    brain.screen.clear_screen()
-    brain.screen.print("autonomous code")
-    # place automonous code here
-"""
 def user_control():
     brain.screen.clear_screen()
-    controller = Controller()
-    
-    # place driver control in this while loop
+    controller=Controller()
     while True:
-        left_motor.set_velocity(controller.axis3.position(), PERCENT)
-        right_motor.set_velocity(controller.axis2.position(), PERCENT)
-        left_motor.spin(FORWARD)
-        right_motor.spin(FORWARD)
+        left_motor_a.set_velocity(controller.axis3.position(), PERCENT)
+        left_motor_a.spin(REVERSE)
+        left_motor_b.set_velocity(controller.axis3.position(), PERCENT)
+        left_motor_b.spin(REVERSE)
+
+        right_motor_a.set_velocity(controller.axis2.position(), PERCENT)
+        right_motor_a.spin(REVERSE)
+        right_motor_b.set_velocity(controller.axis2.position(), PERCENT)
+        right_motor_b.spin(REVERSE)
+       
         wait(5, MSEC)
 
-        # move forward
-        if controller.buttonL1.pressing():
-            intake_motor.spin(FORWARD)
-        # move backwards
-        elif controller.buttonL2.pressing():
-            intake_motor.spin(REVERSE)
-        # stop moving
-        else:
-            intake_motor.stop()
-
-        # move conveyor belt up (pick up or placement into high tube)
+        # move conveyor belt up (pick up or place ball into high tube)
+        conveyor.set_velocity(100, PERCENT)
+        conveyor_2.set_velocity(100, PERCENT)
         if controller.buttonR1.pressing():
-            conveyor_motor.spin(FORWARD)
+            conveyor.spin(FORWARD)
+            conveyor_2.spin(FORWARD)
         # place ball into lower tube
         elif controller.buttonR2.pressing():
-            conveyor_motor.spin(REVERSE)
+            conveyor.spin(REVERSE)
+            conveyor_2.spin(REVERSE)
         else:
             # stop conveyor belt
-            conveyor_motor.stop()
+            conveyor.stop()
+            conveyor_2.stop()
         wait(20, MSEC)
-        
 
-# create competition instance
 user_control()
